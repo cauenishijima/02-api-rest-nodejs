20eda5c76fe95b141c1d9bdb41c65f6f132194e6
Cauê Nishijima <caue1987@gmail.com>
2023-07-16 16:04:47 -0300

    Route to create a new transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,26 @@
-import { FastifyInstance } from "fastify"
+import { FastifyInstance, FastifyRegister, FastifyReply, FastifyRequest } from "fastify"
 import { knex } from "../database"
+import { z } from "zod"
+import { randomUUID } from "crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async (request, reply) => {
-    const transactions = await knex('transactions').
-    where('amount', 1000).select('*')
-  
-    return transactions
+  app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.coerce.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1
+    })
+
+    reply.status(201).send();
+
   })
 }
