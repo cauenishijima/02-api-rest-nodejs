deb1cd35ba30078a17bc348821f25be9f68b5b87
Cauê Nishijima <caue1987@gmail.com>
2023-07-16 12:53:35 -0300

    Config dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { randomUUID } from 'node:crypto';
 
 const app = fastify();
 
 app.get('/hello', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transactions = await knex('transactions').
+  where('amount', 1000).select('*')
 
-  return tables;
+  return transactions
 })
 
 app.listen({
